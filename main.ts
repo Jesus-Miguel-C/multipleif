24864527bcc032f4ac393d0a45fd00ccbed5fcc3
Jesus-Miguel-C <113448418+Jesus-Miguel-C@users.noreply.github.com>
2022-10-18 17:02:30 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.x < scene.screenWidth() / 2) {
+        info.changeScoreBy(1)
+    }
+    if (mySprite.y < scene.screenHeight() / 2) {
+        info.changeLifeBy(-1)
+    }
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f f e e e e e e f f . 
+    f f f f 4 e e e f f f f 
+    f f f 4 4 4 e e f f f f 
+    f f f 4 4 4 4 e e f f f 
+    f 4 e 4 4 4 4 4 4 e 4 f 
+    f 4 4 f f 4 4 f f 4 4 f 
+    f e 4 d d d d d d 4 e f 
+    . f e d d b b d d e f . 
+    . f f e 4 4 4 4 e f f . 
+    e 4 f b 1 1 1 1 b f 4 e 
+    4 d f 1 1 1 1 1 1 f d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+info.setLife(5)
